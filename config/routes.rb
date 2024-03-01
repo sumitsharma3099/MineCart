bf8a6b60cd4d29936cc4a94be376df08158820a7
Sumit <ss828863@gmail.com>
2024-03-01 14:41:50 +0530

    Some changes in code and add welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,31 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins, :skip => [:registrations, :passwords]
+  # Default root path for non-logged-in users
+  unauthenticated do
+    root 'welcome#index'
+  end
 
-  # User routes
+  # Root path for logged-in non-admin users
   authenticated :user do
     root 'stores#index', as: :user_root
   end
 
-  # Admin routes
+  # Root path for admin users
   authenticated :admin do
     root 'admins#dashboard', as: :admin_root
   end
+
+  devise_for :users
+  devise_for :admins, skip: [:registrations, :passwords]
 
   namespace :admin do
     resources :products, except: :show
     resources :vendors, only: :index
   end
 
-  get '/add_product_to_cart/:id', to: "carts#add_product", as: "add_product_to_cart"
-  get '/place_order', to: "carts#place_order", as: "place_order"
+  get '/add_product_to_cart/:id', to: 'carts#add_product', as: 'add_product_to_cart'
+  get '/place_order', to: 'carts#place_order', as: 'place_order'
   resources :carts, only: [:index, :destroy]
 
+  # Route for handling unmatched paths
   match '*unmatched', to: 'application#not_found_method', via: :all
-
 end
